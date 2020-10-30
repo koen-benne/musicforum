3eaf247101b4f7d8a76e272156dad2dbbb03a566
koen-benne <57910795+koen-benne@users.noreply.github.com>
2020-10-30 13:02:54 +0100

    Added edit page for posts

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,61 @@
+@extends('app')
+
+@section('title', 'edit posts')
+
+@section('content')
+<div class="form-container">
+    <div class="form-header">Edit Post</div>
+
+    <div class="form-body">
+        <form method="POST" action="{{ route('posts.store') }}" enctype="multipart/form-data">
+            @csrf
+
+            <div class="form-group">
+                <label class="form-label" for="title">Title</label>
+
+                <div>
+                    <input id="title" type="text" class="form-textbox @error('title') is-invalid @enderror" name="title" value="{{ old('title') ?: $post->title }}" required autocomplete="title" autofocus>
+
+                    @error('title')
+                    <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group">
+
+                <input id="file" type="file" class="form-file @error('title') is-invalid @enderror" name="file" value="{{  old('file') }}" required autocomplete="file" autofocus>
+
+                @error('file')
+                <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                @enderror
+
+            </div>
+
+            <div class="form-group">
+                <label class="form-label" for="description">Description</label>
+
+                <div>
+                    <textarea id="description" class="form-textarea @error('description') is-invalid @enderror"  rows="10" name="description" value="{{ old('description') ?: $post->description }}" required autocomplete="description"></textarea>
+
+                    @error('description')
+                    <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group">
+                <button type="submit" class="form-submit">
+                    Edit
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
